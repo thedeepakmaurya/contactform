c8996325f1130ea4cb3563742911c5ca7031c07e
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-06-06 22:40:45 +0530

    fixed layout

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,7 +8,7 @@ const Button = ({ step }) => {
     <div className='flex bg-indigo-50 sm:w-[92%] w-[55%] h-14 rounded-t-xl sm:pr-2'>
       {steps.map((stepName, index) => (
         <div
-          className={`flex items-center justify-center w-full h-full ${step === index + 1 ? "bg-indigo-600 rounded-r-full " : index < step ? 'bg-indigo-600 rounded-r-none ' : ''}`} key={index}>
+          className={`flex items-center justify-center w-full  h-full ${step === index + 1 ? "bg-indigo-600 rounded-tl-xl rounded-r-full  " : index < step ? 'bg-indigo-600 rounded-tl-xl rounded-r-none ' : ''}`} key={index}>
           <div className={`sm:text-[8px] flex justify-center sm:w-3 sm:h-3 w-5 h-5 text-xs mr-2 items-center rounded-full ${step === index + 1 ? 'bg-white text-indigo-600' : 'bg-indigo-300 text-white'}`}>
             {index + 1}
           </div>
--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="flex items-center justify-center sm:w-[92%] w-[55%] relative mb-10 text-white">
             <h1 className="sm:text-lg text-xl font-semibold">
                 Create New Account
-                <span className='sm:text-xs text-sm absolute right-0 top-1'>Contact Us</span>
+                <span className='sm:hidden sm:text-xs text-sm absolute right-0 top-1'>Contact Us</span>
             </h1>
         </div>
     )
